ba93553e46f6ed950d98638834cd8a24fd6abd71
onderveli <onderveli123@gmail.com>
2017-02-27 01:19:42 +0300

    play buton onclick

--- app/src/main/java/com/yazilimciakli/oneway/MainActivity.java
+++ app/src/main/java/com/yazilimciakli/oneway/MainActivity.java
@@ -1,13 +1,10 @@
 package com.yazilimciakli.oneway;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.ActionMenuItemView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.yazilimciakli.oneway.Utils.Calligrapher;
 
@@ -28,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
         Calligrapher calligrapher = new Calligrapher(this);
         calligrapher.setFont(this, FONT, true);
 
+        Button btnPlay=(Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         Button btnExit = (Button) findViewById(R.id.btnExit);
         btnExit.setOnClickListener(new View.OnClickListener() {
