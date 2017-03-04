2cee51c4c80710dc36b15c87e26e154aec5ec3dc
Mehmet <mmtkalayci@gmail.com>
2017-03-05 02:35:05 +0300

    LevelItem yapılmaya başlandı

---
+++ app/src/main/java/com/yazilimciakli/oneway/Controls/LevelItem.java
@@ -0,0 +1,16 @@
+package com.yazilimciakli.oneway.Controls;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LevelItem extends View {
+
+    boolean star1, star2, star3;
+    String levelText =  "1";
+
+    public LevelItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
