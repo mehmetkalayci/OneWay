685980f3f2c07969038c936d97229428a4d430d3
onderveli <onderveli123@gmail.com>
2017-03-05 21:55:55 +0300

    Merge branch 'master' of C:\Users\Admin\Desktop\OneWay with conflicts.

--- app/src/main/java/com/yazilimciakli/oneway/Controls/LevelItem.java
+++ app/src/main/java/com/yazilimciakli/oneway/Controls/LevelItem.java
@@ -2,15 +2,58 @@ package com.yazilimciakli.oneway.Controls;
 
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class LevelItem extends View {
+import com.yazilimciakli.oneway.R;
 
-    boolean star1, star2, star3;
-    String levelText =  "1";
+public class LevelItem extends BaseAdapter {
 
-    public LevelItem(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    Context cntx;
+    String titles[];
+    LayoutInflater inflater;
+
+    public LevelItem(Context context,String[] title)
+    {
+        titles=title;
+        cntx=context;
+    }
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView title;
+        ImageView str1,str2,str3;
+
+        inflater = (LayoutInflater) cntx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.level_item, parent, false);
+        title = (TextView) itemView.findViewById(R.id.levelText);
+
+        str1=(ImageView) itemView.findViewById(R.id.star1);
+        str2=(ImageView) itemView.findViewById(R.id.star2);
+        str3=(ImageView) itemView.findViewById(R.id.star3);
+
+        str1.setImageResource(R.drawable.ic_star_gold_48dp);
+        str2.setImageResource(R.drawable.ic_star_gold_48dp);
+        title.setText(titles[position]);
+        return itemView;
     }
 }
