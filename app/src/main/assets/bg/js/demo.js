bcd39d1e45bfe98324b54fe2294f1584bc3bcfa8
Mehmet <mmtkalayci@gmail.com>
2017-03-29 01:22:51 +0300

    Webview animasyonu silindi (bg dosyası)

--- app/src/main/assets/bg/js/demo.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    particleground(document.getElementById('particles'), {
-        dotColor: '#ffffff',
-        lineColor: '#f7f7f7'
-    });
-}, false);
